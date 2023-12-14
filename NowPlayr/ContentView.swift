0c44b5d264ed657f52a77d4eae52e8656d26e1f7
CozyBrian <dragonbrian33@gmail.com>
2023-12-14 16:36:46 +0000

    Initial Commit

---
+++ NowPlayr/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NowPlayr
+//
+//  Created by Brian Newton on 12/14/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NowPlayr/NowPlayrApp.swift
@@ -0,0 +1,17 @@
+//
+//  NowPlayrApp.swift
+//  NowPlayr
+//
+//  Created by Brian Newton on 12/14/23.
+//
+
+import SwiftUI
+
+@main
+struct NowPlayrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
